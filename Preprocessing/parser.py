69c3c82d49d1c60ed1e683941441b5c4c0562940
ggreiner333 <ggreiner@trinity.edu>
2023-07-04 14:45:30 -0500

    changes

--- Preprocessing/parser.py
+++ Preprocessing/parser.py
@@ -9,7 +9,7 @@ from end2end_alphaPowerandiAPF import end2end_alphaPowerandiAPF
 
 varargs = {}
 
-main_dir = '/data/zhanglab/ggreiner/MENTAL/TDBRAIN'
+main_dir = '/data/zhanglab/ggreiner/MENTAL/TDBRAIN/'
 
 varargs[      'sourcepath'] =  main_dir + 'derivatives'
 print('Reading data from: '+ varargs['sourcepath'])
