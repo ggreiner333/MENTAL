f068b1701919c1ee84a3be9464066af742a306eb
ggreiner333 <ggreiner@trinity.edu>
2023-07-04 12:21:28 -0500

    added particpants csv

--- Preprocessing/parser.py
+++ Preprocessing/parser.py
@@ -9,19 +9,22 @@ from end2end_alphaPowerandiAPF import end2end_alphaPowerandiAPF
 
 varargs = {}
 
-varargs['sourcepath'] =  'C:/Users/glgre/Downloads/TD-BRAIN-SAMPLE/derivatives/'
-print('Reading data from: '+ 'C:/Users/glgre/Downloads/TD-BRAIN-SAMPLE/derivatives/')
-varargs['participantspath'] = 'C:/Users/glgre/Downloads/TD-BRAIN-SAMPLE'
-print('Reading data from: '+ 'C:/Users/glgre/Downloads/TD-BRAIN-SAMPLE')
+main_dir = 'C:/Users/glgre/Downloads/TD-BRAIN-SAMPLE/'
 
-varargs['preprocpath'] = varargs['sourcepath']+'preprocessed'
+varargs[      'sourcepath'] =  main_dir + 'derivatives'
+print('Reading data from: '+ varargs['sourcepath'])
+varargs[     'preprocpath'] =  main_dir + 'preprocessed'
+varargs['participantspath'] =  main_dir
+print('Reading data from: '+ varargs['participantspath'])
+
 if not os.path.exists(varargs['preprocpath']):
     os.mkdir(varargs['preprocpath'])
-print('Writing preprocessed data to: '+varargs['preprocpath'])
-varargs['resultspath'] = varargs['sourcepath']+'results_manuscript'
+print('Writing preprocessed data to: ' + varargs['preprocpath'])
+
+varargs['resultspath'] = main_dir + 'results_manuscript'
 if not os.path.exists(varargs['resultspath']):
     os.mkdir(varargs['resultspath'])
-print('Writing results to: '+varargs['resultspath'])
+print('Writing results to: ' + varargs['resultspath'])
 
 varargs['condition']=['EO','EC']
 varargs['chans']='Pz'
