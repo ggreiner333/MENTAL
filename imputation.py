e23a1fded21eb715d7934f86c99ac43d911656b1
ggreiner333 <ggreiner@trinity.edu>
2023-07-06 11:34:38 -0500

    tester

--- imputation.py
+++ imputation.py
@@ -65,7 +65,7 @@ def generate_samples(ptc, psd, out):
     files = os.listdir(loc)
     for f in files:
         pth = os.path.join(loc,f)
-        loaded = np.load(pth)
+        loaded = np.loadtxt(pth, allow_pickle=True)
         print(loaded)
 
 generate_samples(ptc_path, psd_path, out_path)
