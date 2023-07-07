7ea401eed48a40f243a4564e314c4707dff5919e
ggreiner333 <ggreiner@trinity.edu>
2023-07-07 15:54:09 -0500

    bug fix

--- clean_data.py
+++ clean_data.py
@@ -139,7 +139,6 @@ def separate_missing_samples(ptc, psd, out):
 
                     # Combine survey and PSD data
                     combined = np.asarray(np.concatenate((ind,psds)), dtype=object)
-                    print(combined)
                     if(combined.__contains__('-1')):
                         missing_samples.append(combined)
                     else:
