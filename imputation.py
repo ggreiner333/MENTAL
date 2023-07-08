881bb7799af936cfb2aea7eb3b7aebdf054be1ee
ggreiner333 <ggreiner@trinity.edu>
2023-07-08 17:59:45 -0500

    first large train test for imputation

--- imputation.py
+++ imputation.py
@@ -33,7 +33,10 @@ my_encoder = VAE(7864, 128)
 optimizer = torch.optim.Adam(my_encoder.parameters(), lr=1e-2, weight_decay=1e-9)
 
 
-epochs = 1000
+for i in range(0, mis_dataset.__len__()):
+    print(mis_dataset.__getitem__(i))
+
+epochs = 0
 
 
 for epoch in range(epochs):
@@ -54,6 +57,7 @@ for epoch in range(epochs):
         print(" Loss: " + str(res) )
         print("-----------------------")
 
-predictions = my_encoder(torch.from_numpy(mis_dataset.__getIndividuals__()))
+#predictions = my_encoder(torch.from_numpy(mis_dataset.__getIndividuals__()))
 
-print(predictions)
+
+#print(predictions)
