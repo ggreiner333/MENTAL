9801b87775b1860f1278c494c75a886ca877a7e5
ggreiner333 <ggreiner@trinity.edu>
2023-07-10 16:47:43 -0500

    first rnn and attention layer push

---
+++ Model/recur.py
@@ -0,0 +1,43 @@
+import os
+from os.path import dirname
+from pathlib import Path
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+import torch
+import torch.nn as nn
+
+import mne
+
+from attentionLayer import AttentionLayer
+
+
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+class EegRNN(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        self.attention = AttentionLayer()
+
+        self.layer_1 = nn.GRU(input_size, hidden_size)
+        self.layer_2 = nn.GRU(hidden_size, output_size)
+        self.output = nn.Sequential(
+            nn.ReLU(),
+            nn.Linear(output_size, 1)
+        )
+
+    def forward(self, eeg, neo, h):
+        x = eeg
+        res, h1 = self.layer_1(  x, h[0])
+        res, h2 = self.layer_2(res, h[1])
+        return self.output(res), (h1, h2)
+        
