844d96c2bb9d77d485247bec6ba52890ea1a2697
ggreiner333 <ggreiner@trinity.edu>
2023-07-11 10:00:37 -0500

    testing

--- Model/attentionLayer.py
+++ Model/attentionLayer.py
@@ -35,9 +35,7 @@ class AttentionLayer(nn.Module):
         k = self.W_k(k.transpose(0,1))
         v = self.W_v(v.transpose(0,1))
 
-        print(k.size())
         k = k.transpose(0,1)
-        print(k.size())
 
         res = torch.matmul(q,k) / math.sqrt(self.dk)
         res = self.softmax(res)
