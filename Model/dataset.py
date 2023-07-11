5ff1ac98696d59ea22661fe1a09d5fcdb80b4fda
ggreiner333 <ggreiner@trinity.edu>
2023-07-11 09:20:21 -0500

    testing for first train

--- Model/dataset.py
+++ Model/dataset.py
@@ -26,3 +26,27 @@ class MultiModalDataset(data.Dataset):
         for i in range(1, self.__len__()):
             res.append((self.individuals[i])[1:])
         return np.array(res)
+    
+
+class SplitDataset(data.Dataset):
+
+    def __init__(self, individuals_file, directory):
+        self.directory = directory
+        self.individuals = np.loadtxt(os.path.join(directory, individuals_file), delimiter=",", dtype="float32")
+        
+    def __len__(self):
+        return np.size(self.individuals, axis=0)
+
+    def __getitem__(self, idx):
+        individual = self.individuals[idx]
+        indication = individual[1]
+        dem_val = individual[2:5]
+        neo_val = individual[5:65]
+        psd_val = individual[65:]
+        return dem_val, neo_val, psd_val, indication
+    
+    def __getIndividuals__(self):
+        res = []
+        for i in range(0, self.__len__()):
+            res.append((self.__getitem__(i))[0])
+        return np.array(res)
--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import mne
 
 
 from Model.dataset import MultiModalDataset
+from Model.dataset import SplitDataset
 from Model.mentalModel import MENTAL
 
 
@@ -23,6 +24,10 @@ test = np.loadtxt(os.path.join('TDBRAIN', 'complete_samples_EC.csv'), delimiter=
 mm_dataset = MultiModalDataset('complete_samples_EC.csv', 'TDBRAIN')
 dataset_loader = data.DataLoader(mm_dataset, batch_size=20, shuffle=True)
 
+main_dataset = SplitDataset('complete_samples_EC.csv', 'TDBRAIN')
+main_loader  = data.DataLoader(main_dataset, batch_size=20, shuffle=True)
+
+
 my_mental = MENTAL(60, 30, 15)
 
 optimizer = torch.optim.Adam(my_mental.parameters(), lr=1e-7, weight_decay=1e-9)
@@ -31,12 +36,12 @@ epochs = 1000
 
 for epoch in range(epochs):
 
-    for (entry, label) in dataset_loader:
+    for (d_entry, n_entry, p_entry, label) in main_loader:
 
-        output = my_mental.forward(entry)
+        output = my_mental.forward(n_entry, p_entry, p_entry, d_entry)
 
         loss = torch.nn.MSELoss()
-        res = loss(output, entry)
+        res = loss(output, label)
 
         optimizer.zero_grad()
         res.backward()
