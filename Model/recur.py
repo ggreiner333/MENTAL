93bade1edf02846e0139fffc6fc28b61893fdaf9
ggreiner333 <ggreiner@trinity.edu>
2023-07-11 10:41:55 -0500

    testing

--- Model/recur.py
+++ Model/recur.py
@@ -32,7 +32,7 @@ class EegRNN(nn.Module):
         )
 
     def forward(self, x, h):
-        x.reshape(20,1,60)
+        x.unsqueeze_(-1)
         print(x.size(axis=1))
         res, h1 = self.layer_1(  x, h[0])
         res, h2 = self.layer_2(res, h[1])
