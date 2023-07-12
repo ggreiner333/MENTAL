25b6ec560c30c7ef4d52cf2feb58082ed1e37aab
ggreiner333 <ggreiner@trinity.edu>
2023-07-12 14:28:07 -0500

    first train with updated model

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ my_mental = MENTAL(60, 3, 3, batch)
 
 optimizer = torch.optim.Adam(my_mental.parameters(), lr=1e-6, weight_decay=1e-9)
 
-epochs = 1
+epochs = 1000
 
 for epoch in range(epochs):
 
