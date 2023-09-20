ce28e7de5422b78371787c15d7a8515907f38c0d
ggreiner333 <ggreiner@trinity.edu>
2023-09-20 14:07:46 -0500

    testing

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ for epoch in range(epochs):
         h1 = h1.transpose(0,1)
         h1 = h1.squeeze(-1)
 
-        label.unsqueeze(1)
+        label = np.reshape(label, (20,1,36))
         print(label.shape)
 
         h = (h0,h1)
