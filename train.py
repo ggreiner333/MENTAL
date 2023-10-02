4a0659fae49234e4e708922f6a7189823d9e8344
ggreiner333 <ggreiner@trinity.edu>
2023-10-02 17:14:51 -0500

    adding 2nd layer back

--- train.py
+++ train.py
@@ -50,23 +50,23 @@ for epoch in range(epochs):
 
     for (d_entry, n_entry, p_entry, label) in train_loader:
 
-        h = d_entry
+        h = (d_entry, d_entry)
 
-        h.unsqueeze_(-1)
-        h0 = h.transpose(1,2)
+        h[0].unsqueeze_(-1)
+        h0 = h[0].transpose(1,2)
         h0 = h0.transpose(0,1)
 
-        #h[1].unsqueeze_(-1)
-        #h1 = h[1].transpose(1,2)
-        #h1 = h1.transpose(0,1)
-        #h1 = h1.squeeze(-1)
+        h[1].unsqueeze_(-1)
+        h1 = h[1].transpose(1,2)
+        h1 = h1.transpose(0,1)
+        h1 = h1.squeeze(-1)
 
-        #h = (h0,h1)
+        h = (h0,h1)
 
         label = np.reshape(label, (20,1,1))
 
         for p in p_entry:
-            output, h0 = my_mental.forward(p, n_entry, h0)
+            output, h = my_mental.forward(p, n_entry, h)
         
         loss = torch.nn.BCELoss()
         res = loss(output, label)
@@ -78,23 +78,23 @@ for epoch in range(epochs):
     if((epoch!=0) and epoch%50==0):
         correct = 0
         for (d_entry, n_entry, p_entry, label) in test_loader:
-            h = d_entry
+            h = (d_entry, d_entry)
 
-            h.unsqueeze_(-1)
-            h0 = h.transpose(1,2)
+            h[0].unsqueeze_(-1)
+            h0 = h[0].transpose(1,2)
             h0 = h0.transpose(0,1)
 
-            #h[1].unsqueeze_(-1)
-            #h1 = h[1].transpose(1,2)
-            #h1 = h1.transpose(0,1)
-            #h1 = h1.squeeze(-1)
+            h[1].unsqueeze_(-1)
+            h1 = h[1].transpose(1,2)
+            h1 = h1.transpose(0,1)
+            h1 = h1.squeeze(-1)
 
-            #h = (h0,h1)
+            h = (h0,h1)
 
             label = np.reshape(label, (20,1,1))
 
             for p in p_entry:
-                output, h0 = my_mental.forward(p, n_entry, h0)
+                output, h = my_mental.forward(p, n_entry, h)
 
             out = output.squeeze_(1)
             preds = []
@@ -134,23 +134,23 @@ for epoch in range(epochs):
 correct = 0
 for (d_entry, n_entry, p_entry, label) in test_loader:
    
-    h = d_entry
+    h = (d_entry, d_entry)
 
-    h.unsqueeze_(-1)
-    h0 = h.transpose(1,2)
+    h[0].unsqueeze_(-1)
+    h0 = h[0].transpose(1,2)
     h0 = h0.transpose(0,1)
 
-    #h[1].unsqueeze_(-1)
-    #h1 = h[1].transpose(1,2)
-    #h1 = h1.transpose(0,1)
-    #h1 = h1.squeeze(-1)
+    h[1].unsqueeze_(-1)
+    h1 = h[1].transpose(1,2)
+    h1 = h1.transpose(0,1)
+    h1 = h1.squeeze(-1)
 
-    #h = (h0,h1)
+    h = (h0,h1)
 
     label = np.reshape(label, (20,1,1))
 
     for p in p_entry:
-        output, h0 = my_mental.forward(p, n_entry, h0)
+        output, h = my_mental.forward(p, n_entry, h)
 
     out = output.squeeze_(1)
     preds = []
@@ -185,7 +185,7 @@ strs.append(str1)
 strs.append(str2)
 strs.append(str3)
 
-with open('out_1e3_1e8.txt', 'w') as f:
+with open('out_2layer_1e5_1e8.txt', 'w') as f:
     for line in strs:
         f.write(line)
         f.write('\n')
