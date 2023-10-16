19ad0c11cad98b37f1ecabc3aa6245b44c67ed8c
ggreiner333 <ggreiner@trinity.edu>
2023-10-16 00:35:08 -0500

    testing

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ def run_train(learn_rate, wd, outfile):
                 #print(label)
                 conds = []
                 for i in range(0, len(label)):
-                    conds.append(label[i][0])
+                    conds.append(label[i].item())
                 print(conds)
                 print(preds)
                 for i in range(0, len(conds)):
