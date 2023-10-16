45f4d4dde94be6d54765f38e493c4ecfe1c62e4c
ggreiner333 <ggreiner@trinity.edu>
2023-10-16 00:32:04 -0500

    testing

--- train.py
+++ train.py
@@ -101,7 +101,7 @@ def run_train(learn_rate, wd, outfile):
             res.backward()
             optimizer.step()
         
-        if((epoch!=0) and epoch%10==0):
+        if((epoch!=0) and epoch%1==0):
             correct = 0
             for (h_entry, n_entry, p_entry, label) in test_loader:
 
