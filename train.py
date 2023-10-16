821a3eb5d4f86bf6457e1bcaf1b168fd4a25a868
ggreiner333 <ggreiner@trinity.edu>
2023-10-15 23:58:14 -0500

    testing

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ def run_train(learn_rate, wd, outfile):
 
             for p in p_entry:
                 print(p[0])
-                print(p[0].numpy().asarray())
+                print(p[0].numpy().array())
 
             print(p_entry.size())
             for p in p_entry:
