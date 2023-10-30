0c989741aacf59f39bf1cd5c055fd56478f60534
ggreiner333 <ggreiner@trinity.edu>
2023-10-29 21:59:15 -0500

    test

--- Model/attentionLayer.py
+++ Model/attentionLayer.py
@@ -47,10 +47,10 @@ class AttentionLayer(nn.Module):
         #k_T = k_res.transpose(0,1)
 
         print("Q size: " + str(q_res.size()))
-        print("K size: " + str(k_T.size()))
+        print("K size: " + str(k.size()))
         print("V size: " + str(v_res.size()))
 
-        res = torch.matmul(q_res / math.sqrt(self.dk), k_T)
+        res = torch.matmul(q_res / math.sqrt(self.dk), k)
         attn_weights = self.dropout(F.softmax(res, dim=-1))
 
         out = torch.matmul(attn_weights, v_res)
