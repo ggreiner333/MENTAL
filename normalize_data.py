30729bde8c2992eb99bbd6e9f045a6ae7e53c957
ggreiner333 <ggreiner@trinity.edu>
2023-11-01 17:53:45 -0500

    info mining

--- normalize_data.py
+++ normalize_data.py
@@ -29,13 +29,14 @@ def normalize_data(path="/data/zhanglab/ggreiner/MENTAL/TDBRAIN"):
 
     # Load Data
     inds = np.load(os.path.join(path, 'small_complete_samples_EC_depression.npy'))
-    print(inds[1].size)
-    print(inds[0].size)
+
     normalized = np.zeros_like(inds)
-    print(normalized.shape)
-    print(inds.shape)
-    #for i in range(0,5):
-        #z_scored = stats.zscore(a, axis=None)
+
+    for i in range(0,10):
+        test = inds[:, i]
+        print(test)
+        z_scored = stats.zscore(test, axis=None)
+        print(z_scored)
 
 normalize_data()
     
