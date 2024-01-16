492dbe1170ce6a71d4e224fb09339cc64b9e3ccf
ggreiner333 <ggreiner@trinity.edu>
2024-01-16 11:31:36 -0600

    test

---
+++ collectEEG.py
@@ -0,0 +1,57 @@
+import os
+from os.path import dirname
+from pathlib import Path
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+import mne
+
+# path of preprocessed EEG data
+preprocess_file_path = 'TDBRAIN/preprocessed'
+
+# path of directory where we will save the PSD features
+psds_path = 'TDBRAIN/PSD_all'
+
+# path of TDBRAIN
+participants_path = 'TDBRAIN'
+
+# out path
+out_path = 'TDBRAIN'
+
+def generate_EC_Samples(ptc, psd, out):
+    survey = np.loadtxt(os.path.join(ptc, "cleaned_participants.csv"), delimiter=",", dtype=str)
+
+    complete_samples = []
+
+    for ind in survey[1:]:
+        
+        # Only consider individuals that we have survey data for
+        # This means excluding the individuals marked for replication
+        id = ind[0]
+        if(not id[0] == '1'):
+
+            # Navigate to the directory with the psd information
+            loc = os.path.join(psd, id)
+            files = os.listdir(loc)
+
+            sn = ind[1]
+            for f in files:
+                if(f.__contains__("EC") and f.__contains__(sn)):
+                    found = True
+                    # Load the PSD values from the files
+                    pth = os.path.join(loc,f)
+                    psds = np.load(pth, allow_pickle=True)
+                    psds = np.squeeze(psds)
+                    psds = psds.flatten()
+
+            indication = [ind[2]]
+            print(psds)
+            complete_samples.append(np.concatenate(indication, psds))
+            
+            
+    all_complete_samples = np.array(complete_samples)
+    np.save(os.path.join(out,'only_EC_samples'), all_complete_samples)
+
+    print("   Total samples: " + str(survey.shape[0]))
+    print("Complete samples: " + str(all_complete_samples.shape[0]))
