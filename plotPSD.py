d6cb1662dcfb74ef5f5e047cf1795a5c87531cdb
ggreiner333 <ggreiner@trinity.edu>
2024-01-29 14:35:33 -0600

    testing plot PSD

---
+++ plotPSD.py
@@ -0,0 +1,33 @@
+import os
+from os.path import dirname
+from pathlib import Path
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+import mne
+
+
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+# path of preprocessed EEG data
+preprocess_file_path = 'TDBRAIN/preprocessed'
+
+# path of directory where we will save the PSD features
+psds_path = 'TDBRAIN/PSD_all'
+
+
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+
+def get_ind():
+    ec_psds = np.load('/data/zhanglab/ggreiner/MENTAL/TDBRAIN/small_complete_samples_EC_depression.npy', allow_pickle=True)
+
+    for ind in ec_psds[0:20]:
+        print(ind)
+
+get_ind()
