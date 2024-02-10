57e9ddb5cafe82ed986f73816ac195d497bf3111
ggreiner333 <ggreiner@trinity.edu>
2024-02-10 01:05:55 -0600

    testing eeg only ADHD

--- new_train_EEG.py
+++ new_train_EEG.py
@@ -422,7 +422,7 @@ def run_EO():
             run_train_eo(learn_rate=learn, wd=weight_decay, batch_sz=batches[j], epochs=epoch[i], 
                     outfile="eo_epoch1000_b15_w6_l3")
 
-run_EO()
+#run_EO()
 
 def run_train_both(learn_rate, wd, batch_sz, epochs, outfile):
     diagnoses = ['-1', 'HEALTHY', 'MDD', 'ADHD', 'SMC', 'OCD', 'TINNITUS', 'INSOMNIA', 'PARKINSON', 'DYSLEXIA',
@@ -633,4 +633,4 @@ def run_EC_EO():
             run_train_both(learn_rate=learn, wd=weight_decay, batch_sz=batches[j], epochs=epoch[i], 
                     outfile="ec_eo_epoch1000_b15_w6_l3")
             
-#run_EC_EO()
+run_EC_EO()
