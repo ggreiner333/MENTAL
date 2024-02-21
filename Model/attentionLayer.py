98b3c009dcac67d626c40f8b6aac1d468e938740
ggreiner333 <ggreiner@trinity.edu>
2024-02-21 14:03:06 -0600

    checking dim sizes

--- Model/attentionLayer.py
+++ Model/attentionLayer.py
@@ -36,23 +36,12 @@ class AttentionLayer(nn.Module):
         self.batch = batch
 
     def forward(self, q, k, v):
-        #print("Q size: " + str(q.size()))
-        #print("K size: " + str(k.size()))
-        #print("V size: " + str(v.size()))
 
         q_res = self.W_q(q)
         k_res = self.W_k(k)
         v_res = self.W_v(v)
 
-        print("Q size: " + str(q_res.size()))
-        print("K size: " + str(k_res.size()))
-        print("V size: " + str(v_res.size()))
-
         k_T = k_res.transpose(1,2)
-
-        #print("Q size: " + str(q_res.size()))
-        #print("K size: " + str(k_T.size()))
-        #print("V size: " + str(v_res.size()))
 
         res = torch.matmul(q_res / math.sqrt(self.dk), k_T)
         attn_weights = F.softmax(res, dim=-1)
@@ -62,7 +51,6 @@ class AttentionLayer(nn.Module):
         result = self.fc(out)
 
         reshaped = torch.reshape(result, [self.batch,60])
-        print(attn_weights.size())
 
         return reshaped, attn_weights
 
