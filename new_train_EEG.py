7d66fd249458576b7d5bf9b46aa1a14fbc18e918
ggreiner333 <ggreiner@trinity.edu>
2024-03-04 01:17:07 -0600

    testing MDD v healthy EEG

--- new_train_EEG.py
+++ new_train_EEG.py
@@ -219,7 +219,7 @@ def run_EC():
             run_train_ec(learn_rate=learn, wd=weight_decay, batch_sz=batches[j], epochs=epoch[i], 
                     outfile="ec_epoch1000_b15_w6_l3")
 
-#run_EC()
+run_EC()
 
 def run_train_eo(learn_rate, wd, batch_sz, epochs, outfile):
     diagnoses = ['-1', 'HEALTHY', 'MDD', 'ADHD', 'SMC', 'OCD', 'TINNITUS', 'INSOMNIA', 'PARKINSON', 'DYSLEXIA',
@@ -630,4 +630,4 @@ def run_EC_EO():
             run_train_both(learn_rate=learn, wd=weight_decay, batch_sz=batches[j], epochs=epoch[i], 
                     outfile="ec_eo_epoch1000_b15_w6_l3")
             
-run_EC_EO()
+#run_EC_EO()
