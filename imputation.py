2bfded54b08639212b84b250af04b68077591bc2
ggreiner333 <ggreiner@trinity.edu>
2024-03-20 22:58:48 -0500

    bug fix

--- imputation.py
+++ imputation.py
@@ -41,7 +41,7 @@ for epoch in range(epochs):
         recon_loss = torch.nn.MSELoss()
         loss = recon_loss(output, vals)
 
-        kl_loss = - torch.sum(1 + torch.log(var.pow(2))-mu.pow(2)-var.pow(2))
+        kl_loss = - torch.sum(1 + torch.log(var)-mu.pow(2)-var.pow(2))
 
         loss = loss + kl_loss
 
