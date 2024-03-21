41bc6f95fc7e686193190926a1594d24de12eac1
ggreiner333 <ggreiner@trinity.edu>
2024-03-20 22:37:14 -0500

    bug fix

--- imputation.py
+++ imputation.py
@@ -44,6 +44,7 @@ for epoch in range(epochs):
         kl_loss = torch.sum(1 + torch.log(var.pow(2))-mu.pow(2)+var.pow(2))
 
         loss = loss + kl_loss
+        print(loss)
 
         optimizer.zero_grad()
         loss.backward()
