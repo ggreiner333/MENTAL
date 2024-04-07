b7f99398470ce508400e9451db9f4f87a78a2417
ggreiner333 <ggreiner@trinity.edu>
2024-04-07 13:29:50 -0500

    bug fix

--- imputation.py
+++ imputation.py
@@ -30,7 +30,7 @@ data_loader = data.DataLoader(complete_dataset, batch_size=5, shuffle=True)
 # Create an instance of the encoder
 encoder = VAE(INPUT_DIM, Z_DIM)
 
-optimizer = torch.optim.Adam(encoder.parameters(), lr=1e-6)
+optimizer = torch.optim.Adam(encoder.parameters(), lr=1e-4)
 
 epochs = 200
 
