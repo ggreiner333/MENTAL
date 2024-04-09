22820597681167c61dce808bb61fdc07c12b9b07
ggreiner333 <ggreiner@trinity.edu>
2024-04-09 14:51:01 -0500

    testing CrossEntropyLoss

--- train_EEG.py
+++ train_EEG.py
@@ -680,7 +680,7 @@ def run_train_EC_Multi(learn_rate, wd, batch_sz, epochs, outfile):
                 loc = 0
                 for j in range(0, 5):
                     if(output[i][j] > mx):
-                        mx = output[i][0][j]
+                        mx = output[i][j]
                         loc = j
                 #if(i==14):
                 #    print(f"row: {output[i][0]}")
