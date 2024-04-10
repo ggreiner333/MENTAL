b186a465cc89c8f3e77830bb27dbb8fcba8ffd69
ggreiner333 <ggreiner@trinity.edu>
2024-04-09 20:57:53 -0500

    testing lower learn rate

--- train_EEG.py
+++ train_EEG.py
@@ -707,10 +707,10 @@ def run_train_EC_Multi(learn_rate, wd, batch_sz, epochs, outfile):
         print(f"Epoch {epoch}: {acc}")
 
         if(epoch%100==0):
-            np.save('/home/ggreiner/MENTAL/TOP5_e3_MENTAL_EC_IMPUTED_ACCS'+str(epoch), accs)
+            np.save('/home/ggreiner/MENTAL/TOP5_e4_MENTAL_EC_IMPUTED_ACCS'+str(epoch), accs)
  
     accs = np.array(accs)
-    np.save('/home/ggreiner/MENTAL/TOP5_e3_MENTAL_EC_IMPUTED_ACCS', accs)
+    np.save('/home/ggreiner/MENTAL/TOP5_e4_MENTAL_EC_IMPUTED_ACCS', accs)
 
 def run_train_EO_Multi(learn_rate, wd, batch_sz, epochs, outfile):
 
@@ -837,10 +837,10 @@ def run_train_EO_Multi(learn_rate, wd, batch_sz, epochs, outfile):
         print(f"Epoch {epoch}: {acc}")
 
         if(epoch%100==0):
-            np.save('/home/ggreiner/MENTAL/TOP5_e3_MENTAL_EO_IMPUTED_ACCS'+str(epoch), accs)
+            np.save('/home/ggreiner/MENTAL/TOP5_e4_MENTAL_EO_IMPUTED_ACCS'+str(epoch), accs)
  
     accs = np.array(accs)
-    np.save('/home/ggreiner/MENTAL/TOP5_e3_MENTAL_EO_IMPUTED_ACCS', accs)
+    np.save('/home/ggreiner/MENTAL/TOP5_e4_MENTAL_EO_IMPUTED_ACCS', accs)
 
 
 # running code
@@ -848,7 +848,7 @@ def run_train_EO_Multi(learn_rate, wd, batch_sz, epochs, outfile):
 epoch = [1000]
 batches = [15]
 
-learn = 1e-3
+learn = 1e-4
 weight_decay = 1e-6
 
 for i in range(0, len(epoch)):
