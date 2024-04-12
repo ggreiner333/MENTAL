f70677e9eacf4dd76171f3897ca84e23c4f0d7d6
ggreiner333 <ggreiner@trinity.edu>
2024-04-12 02:20:44 -0500

    testing creating top3

--- clean_data.py
+++ clean_data.py
@@ -651,7 +651,7 @@ def test_findlen():
     for ind in complete:
         if ((ind[1] > 1) and (ind[1] < 5)):
             top3.append(ind)
-    top3 = np.array(np3)
+    top3 = np.array(top3)
     print(complete.shape)
     print(top3.shape)
 
