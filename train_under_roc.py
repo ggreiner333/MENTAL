845ce18dfd7f23de77e8d5b436bfa2f4ec04bcd0
ggreiner333 <ggreiner@trinity.edu>
2024-09-26 20:59:19 -0600

    roc for eeg only

--- train_under_roc.py
+++ train_under_roc.py
@@ -317,6 +317,6 @@ weight_decay = 1e-6
 
 for i in range(0, len(epoch)):
     for j in range(0, len(batches)):
-        run_train_EC(learn_rate=learn, wd=weight_decay, batch_sz=batches[j], epochs=epoch[i], 
+        run_train_EO(learn_rate=learn, wd=weight_decay, batch_sz=batches[j], epochs=epoch[i], 
                   outfile="tester")
         
